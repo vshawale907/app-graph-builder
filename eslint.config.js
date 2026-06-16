e1a01e5de193c39d52653c00e473517b7a0ce0ca
Vaibhav Shankar Hawale <vaibhavhawale907@gmail.com>
2026-06-16 14:28:57 +0530

    init: scaffold vite + react + ts project with strict mode

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.strict],
+    files: ['**/*.{ts,tsx}'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/consistent-type-imports': 'warn',
+    },
+  },
+)
